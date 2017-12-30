d9964c3135ae78692c92de21f1d8f5056548c9a8
dkallstrom <dave.kallstrom@gmail.com>
2017-12-30 15:29:00 -0600

    address mapping

--- albatross-api/src/main/java/org/tokencreek/application/AlbatrossApplication.java
+++ albatross-api/src/main/java/org/tokencreek/application/AlbatrossApplication.java
@@ -1,28 +1,34 @@
 package org.tokencreek.application;
 
 
+import com.google.common.collect.Sets;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tokencreek.resource.AlbatrossResource;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationPath("/albatross-api")
 public class AlbatrossApplication extends Application {
 
 
+    static final Logger logger = LoggerFactory.getLogger(AlbatrossApplication.class);
+    private static final Set<Class<?>> allResources = Sets.newHashSet();
     private Set<Class<?>> findAllResources(){
-        Reflections reflections = new Reflections("org.tokencreek.resource");
-        return reflections.getTypesAnnotatedWith(AlbatrossResource.class);
+        if(allResources.isEmpty()){
+            Reflections reflections = new Reflections("org.tokencreek.resource");
+            allResources.addAll(reflections.getTypesAnnotatedWith(AlbatrossResource.class));
+            logger.info("Loading Resources {}" , allResources.size());
+        }
+
+        return allResources;
 
     }
     @Override
     public Set<Class<?>> getClasses(){
-        Set<Class<?>> resources = new HashSet();
-        resources.addAll(findAllResources());
-        resources.add(AlbatrossApplication.class);
-        return resources;
+        return findAllResources();
     }
 }
