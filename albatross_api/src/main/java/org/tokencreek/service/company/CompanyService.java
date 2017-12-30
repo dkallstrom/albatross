de55afd122a6f1efdcc58ee68fc1e97dae2dd3f1
dkallstrom <dave.kallstrom@gmail.com>
2017-12-30 11:07:57 -0600

    mapping working better

--- albatross_api/src/main/java/org/tokencreek/service/company/CompanyService.java
+++ albatross_api/src/main/java/org/tokencreek/service/company/CompanyService.java
@@ -3,7 +3,6 @@ package org.tokencreek.service;
 
 import org.tokencreek.Company;
 
-
 public interface CompanyService {
 
     Company getCompanyById(Integer companyId);
--- albatross_api/src/main/java/org/tokencreek/service/company/impl/CompanyServiceImpl.java
+++ albatross_api/src/main/java/org/tokencreek/service/company/impl/CompanyServiceImpl.java
@@ -2,27 +2,32 @@ package org.tokencreek.service;
 
 
 import org.mybatis.cdi.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tokencreek.Company;
 import org.tokencreek.mapper.CompanyMapper;
 import org.tokencreek.qualifiers.AlbatrossMapper;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import java.io.Serializable;
 
 @RequestScoped
-public class CompanyServiceImpl implements CompanyService, Serializable {
+public class CompanyServiceImpl implements CompanyService {
 
+    static final Logger logger = LoggerFactory.getLogger(CompanyServiceImpl.class);
     private CompanyMapper companyMapper;
+
 
     @Inject @AlbatrossMapper
     public CompanyServiceImpl(CompanyMapper companyMapper){
         this.companyMapper = companyMapper;
     }
 
-    @Transactional
-    public Company getCompanyById(Integer companyId){
-        return companyMapper.findCompanyByCompanyId(companyId);
-    }
 
+    @Transactional
+    public Company  getCompanyById(Integer companyId){
+        Company company = this.companyMapper.findCompanyByCompanyId(companyId);
+        logger.info("Company {}", company);
+        return company;
+    }
 }
